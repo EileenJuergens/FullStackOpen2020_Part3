a1bbc10b6d54ad17946b2ee32b6050d17c7099e2
EileenJuergens <juergens.eileen@gmail.com>
2020-04-25 13:37:28 +0200

    feat: finish part 3.1 - phonebook

---
+++ phonebook/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  },
+  {
+    name: "Eileen Juergens",
+    number: "122-879-4534",
+    id: 5
+  }
+]
+
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...persons.map(person => person.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    name: body.content,
+    number: body.content,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
