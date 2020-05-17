457555b4961a702f0494610fc4b2f98e465a1bfa
EileenJuergens <juergens.eileen@gmail.com>
2020-05-17 18:54:36 +0200

    refactor: changes for heroku 12

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const logger = (req, res, next) => {
 app.use(cors());
 app.use(express.static('build'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger)
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :request-body'));
 
